51f6959520e361939c0f3523b25df7a07d798622
Niladri Das <bniladridas@users.noreply.github.com>
2025-11-21 00:09:33 +0530

    fix: remove firefox from playwright config to avoid 404

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,11 +32,6 @@ export default defineConfig({
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
